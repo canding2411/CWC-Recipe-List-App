a28cfd8cfdadccc2787221f67cb8fba8191c95aa
Martin Strzeja <Martin.Strzeja@me.com>
2021-05-01 10:48:15 +0200

    Initial Commit

---
+++ CWC-Recipe List App/CWC_Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_Recipe_List_AppApp.swift
+//  CWC-Recipe List App
+//
+//  Created by Martin Strzeja on 01.05.21.
+//
+
+import SwiftUI
+
+@main
+struct CWC_Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
